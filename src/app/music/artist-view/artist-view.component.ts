910c2f8eb51611df051a91ea6f0586343916ff4c
myflowpl <pedro.blaszczak@gmail.com>
2018-11-28 12:14:35 +0100

    edit artis

--- src/app/music/artist-view/artist-view.component.ts
+++ src/app/music/artist-view/artist-view.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Artist } from 'src/app/models';
 import { Observable } from 'rxjs';
-import { map, switchMap, distinctUntilChanged, delay } from 'rxjs/operators';
+import { map, switchMap, distinctUntilChanged, delay, tap } from 'rxjs/operators';
 import { ArtistService } from '../services/artist.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-artist-view',
@@ -12,27 +13,47 @@ import { ArtistService } from '../services/artist.service';
 })
 export class ArtistViewComponent implements OnInit {
 
+  isEdit = false;
+
   public artist$: Observable<Artist>;
+  public artists$: Observable<Artist[]>;
+
+  artistForm: FormGroup;
 
   constructor(private route: ActivatedRoute, private artistService: ArtistService) { }
 
   ngOnInit() {
 
+
     // this.route.paramMap.subscribe(param => console.log('params', param));
+
+    this.artists$ = this.artistService.getArtists();
 
     this.artist$ = this.route.paramMap.pipe(
 
       map(paramsMap => paramsMap.get('id')),
-
-      distinctUntilChanged(),
-
-      // merge(this.onBtnClick$),
       switchMap((id) => {
-        return this.artistService.getArtist(id); // .pipe(delay(1000));
-      })
+        return this.artistService.getArtist(id);
+      }),
+
+      tap(artist => this.artistForm.patchValue(artist))
 
     );
 
+    this.artistForm = new FormGroup({
+      id: new FormControl(''),
+      name: new FormControl(''),
+      img: new FormControl(''),
+    });
+
+  }
+
+  save() {
+    console.log('save', this.artistForm.value);
+    this.artistService.updateArtist(this.artistForm.value).subscribe(
+      s => console.log('S', s),
+      err => console.log('err', err)
+    );
   }
 
 }
--- src/app/music/services/artist.service.ts
+++ src/app/music/services/artist.service.ts
@@ -2,20 +2,47 @@ import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BASE_URL } from 'src/app/shared/tokens';
 import { Artist } from 'src/app/models';
-import { Observable } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
+import { merge } from 'rxjs';
+import { tap, startWith, switchMap, share } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistService {
 
+  reload$ = new Subject();
+
+  getArtists$ = this.http.get<Artist[]>(this.baseUrl + '/artists').pipe(share());
+
   constructor(private http: HttpClient, @Inject(BASE_URL) private baseUrl: string) { }
 
   getArtists(): Observable<Artist[]> {
-    return this.http.get<Artist[]>(this.baseUrl + '/artists');
+
+    return merge(
+      this.reload$,
+      of(1)
+    ).pipe(
+      switchMap(() => this.getArtists$)
+    );
   }
 
   getArtist(id): Observable<Artist> {
-    return this.http.get<Artist>(this.baseUrl + '/artists/' + id);
+
+    return merge(
+      this.reload$,
+      of(1)
+    ).pipe(
+      switchMap(() => this.http.get<Artist>(this.baseUrl + '/artists/' + id))
+    );
+    // return this.http.get<Artist>(this.baseUrl + '/artists/' + id);
+  }
+
+  updateArtist(artist: Artist): Observable<Artist> {
+
+    return this.http.patch<Artist>(this.baseUrl + '/artists/' + artist.id, artist).pipe(
+      tap(() => this.reload$.next(1))
+    );
+
   }
 }
